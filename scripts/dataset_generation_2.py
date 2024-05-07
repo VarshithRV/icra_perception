edd99271d3b3c09b8de6c4a879c6073d3c3d42a2
Varshith R V <vrv278@gmail.com>
2024-05-07 14:19:36 +0800

    center pixel coordinates estimation

---
+++ scripts/dataset_generation_2.py
@@ -0,0 +1,105 @@
+import cv2
+import numpy as np
+import pandas as pd
+import os
+import sys
+
+################################ DATASET PARAMETERS AND CONSTANTS ################################
+CHEST_CAM_OFFSET = 0.8 # meters, distance from the chest camera to the table
+CHEST_CAM_FOCAL_LENGTH = 0.00304 # meters, focal length of the chest camera
+DON_DEPTH = 0.045 # depth of the bowl
+DON_RADIUS = 0.0575 # radius of the bowl
+ETA  =  0.000008 # magic number
+CENTER_X = 240 # center of the DON 640
+CENTER_Y = 320 # center of the DON 480
+###################################################################################################
+
+
+def value(x,y,center_x, center_y):
+    d = CHEST_CAM_OFFSET - ETA*((x-center_x)**2 + (y-center_y)**2)
+    d = np.random.normal(d, 0.001)
+    if d >= CHEST_CAM_OFFSET - DON_DEPTH:
+        return d
+    else : 
+        return CHEST_CAM_OFFSET
+    
+
+image = np.zeros((480,640))
+
+# generate the depth image
+for x in range(0, 640):
+    for y in range(0, 480):
+        image[y][x] = value(x,y,CENTER_X,120)
+
+image2 = np.zeros((480,640))
+
+# generate the depth image
+for x in range(0, 640):
+    for y in range(0, 480):
+        image2[y][x] = value(x,y,CENTER_X,320)
+
+image = image + image2
+image -= CHEST_CAM_OFFSET
+
+
+# convert the image matrix to a 8-bit image, darker => deeper
+image_prime = 1-(image - np.min(image))/(np.max(image) - np.min(image))
+image_prime = image_prime*255
+image_prime = image_prime.astype(np.uint8)
+
+# show the image
+cv2.imshow('image', image_prime)
+# cv2.waitKey(0)
+# save the image
+cv2.imwrite('don.png', image_prime)
+
+# generate mask for the boundary of the DON
+min_depth = np.min(image)
+max_depth = np.max(image)
+threshold = 0.0001
+
+# generate the mask
+mask1 = np.zeros((480,640))
+mask2 = np.zeros((480,640))
+for x in range(0, 640):
+    for y in range(0, 480):
+        if image[y][x] < CHEST_CAM_OFFSET - (DON_DEPTH - threshold):
+            if y < 240:
+                mask1[y][x] = 1
+            else:
+                mask2[y][x] = 1
+
+# convert the mask matrix to a 8-bit image
+mask1 = mask1*255
+mask1 = mask1.astype(np.uint8)
+mask2 = mask2*255
+mask2 = mask2.astype(np.uint8)
+
+# show the mask
+cv2.imshow('mask', mask1)
+# cv2.waitKey(0)
+cv2.imwrite('don1_mask.png', mask1)
+cv2.imshow('mask', mask2)
+# cv2.waitKey(0)
+cv2.imwrite('don2_mask.png', mask2)
+
+# In the image, there are two DONS, find the centroid of both the dons
+# convert image_prime to color image
+image_prime = cv2.cvtColor(image_prime, cv2.COLOR_GRAY2BGR)
+
+M = cv2.moments(mask1)
+cX = int(M["m10"] / M["m00"])
+cY = int(M["m01"] / M["m00"])
+cv2.circle(image_prime, (cX, cY), 5, (0, 0, 255), -1)
+
+M = cv2.moments(mask2)
+cX = int(M["m10"] / M["m00"])
+cY = int(M["m01"] / M["m00"])
+cv2.circle(image_prime, (cX, cY), 5, (255, 0, 0), -1)
+
+
+# show the image
+cv2.imshow('image', image_prime)
+# save the image
+cv2.imwrite('don_centroid.png', image_prime)
+# cv2.waitKey(0)
